a7712cfb96f48008f1f72e369c9c1880e3ae41fd
Victor Dalosto <victordalosto@gmail.com>
2023-06-19 06:54:13 -0300

    fixing path

--- src/main/java/inframachine/trainer/configuration/ParameterConfiguration.java
+++ src/main/java/inframachine/trainer/configuration/ParameterConfiguration.java
@@ -1,5 +1,6 @@
 package inframachine.trainer.configuration;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,13 +15,26 @@ public class ParameterConfiguration {
 
     @Bean("primaryLayer")
     public List<Layers> primaryLayer() throws StreamReadException, DatabindException, IOException {
-        return Layers.getFirstLayer();
+        List<Layers> layers = new ArrayList<Layers>();
+        layers.add(new Layers("regulamentacao", "red"));
+        layers.add(new Layers("advertencia", "rgb(225, 169, 24)"));
+        layers.add(new Layers("indicativa", "green"));
+        layers.add(new Layers("servicos", "blue"));
+        layers.add(new Layers("educativa", "grey"));
+        layers.add(new Layers("turistico", "brown"));
+        layers.add(new Layers("temporaria", "brown"));
+        layers.add(new Layers("null", "black"));
+        return layers;
     }
 
 
     @Bean("secondaryLayer")
     public List<Layers> secondaryLayer() throws StreamReadException, DatabindException, IOException {
-        return Layers.getSecondLayer();
+        List<Layers> layers = new ArrayList<Layers>();
+        layers.add(new Layers("boa", "green"));
+        layers.add(new Layers("regular", "orange"));
+        layers.add(new Layers("ruim", "red"));
+        return layers;
     }
 
 }
--- src/main/java/inframachine/trainer/model/Layers.java
+++ src/main/java/inframachine/trainer/model/Layers.java
@@ -1,34 +1,15 @@
 package inframachine.trainer.model;
-import java.io.IOException;
-import java.util.List;
-import org.springframework.core.io.ClassPathResource;
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Data
+@AllArgsConstructor
 public class Layers {
 
     private String name;
 
     private String color;
 
-
-
-    public static List<Layers> getFirstLayer() throws IOException, StreamReadException, DatabindException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(new ClassPathResource("layers/primaryLayer.json").getFile(), 
-                                mapper.getTypeFactory().constructCollectionType(List.class, Layers.class));
-    }
-
-
-
-    public static List<Layers> getSecondLayer() throws StreamReadException, DatabindException, IOException {
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(new ClassPathResource("layers/secondaryLayer.json").getFile(), 
-                                mapper.getTypeFactory().constructCollectionType(List.class, Layers.class));
-    }
 
 }
