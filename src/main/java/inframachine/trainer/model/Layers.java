609c448353d17caaf9e4ff14cfacea526313e12c
Victor Dalosto <victordalosto@gmail.com>
2023-06-19 06:28:00 -0300

    fix path resource

--- src/main/java/inframachine/trainer/model/Layers.java
+++ src/main/java/inframachine/trainer/model/Layers.java
@@ -1,7 +1,7 @@
 package inframachine.trainer.model;
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import org.springframework.core.io.ClassPathResource;
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -19,7 +19,7 @@ public class Layers {
 
     public static List<Layers> getFirstLayer() throws IOException, StreamReadException, DatabindException {
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(new File("src/main/layers/primaryLayer.json"), 
+        return mapper.readValue(new ClassPathResource("layers/primaryLayer.json").getFile(), 
                                 mapper.getTypeFactory().constructCollectionType(List.class, Layers.class));
     }
 
@@ -27,7 +27,7 @@ public class Layers {
 
     public static List<Layers> getSecondLayer() throws StreamReadException, DatabindException, IOException {
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(new File("src/main/layers/secondaryLayer.json"), 
+        return mapper.readValue(new ClassPathResource("layers/secondaryLayer.json").getFile(), 
                                 mapper.getTypeFactory().constructCollectionType(List.class, Layers.class));
     }
 
