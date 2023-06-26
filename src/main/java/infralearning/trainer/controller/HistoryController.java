4072c3a97ea5b2455451e5cb8f10e9a9356e08c9
Victor Dalosto <victordalosto@gmail.com>
2023-06-26 07:02:57 -0300

    added security

--- src/main/java/infralearning/trainer/controller/HistoryController.java
+++ src/main/java/infralearning/trainer/controller/HistoryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import infralearning.trainer.model.Layers;
 import infralearning.trainer.model.TableRow;
 import infralearning.trainer.repository.DomainRepository;
 
@@ -14,12 +15,21 @@ public class HistoryController {
     @Autowired
     private DomainRepository repository;
 
+    @Autowired
+    private List<Layers> layers1;
+
+    @Autowired
+    private List<Layers> layers2;
+
 
     @GetMapping("/history")
     public String history(Model model) {
         List<TableRow> table = repository.getTable();
         model.addAttribute("table", table);
         model.addAttribute("totalOfRows", table.stream().mapToLong(TableRow::getCount).sum());
+
+        model.addAttribute("layers1", layers1);
+        model.addAttribute("layers2", layers2);
         return "history";
     }
 }
